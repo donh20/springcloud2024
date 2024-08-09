2d307bbdfff25ecda4218cbf16de184d64ad13bf
haodong <haodong@ncamc.com.cn>
2024-08-09 19:12:36 +0800

    first commit to SpringCloud2024

---
+++ cloud-provider-payment8001/src/main/java/com/ncamc/cloud/entities/PayDTO.java
@@ -0,0 +1,26 @@
+package com.ncamc.cloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * 和前端交互只能暴露DTO，不能暴露ENTITY，因为这样对方会反推后端的表结构
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PayDTO implements Serializable {
+    private Integer id;
+    //支付流水号
+    private String payNo;
+    //订单流水号
+    private String orderNo;
+    //用户账号ID
+    private Integer userId;
+    //交易金额
+    private BigDecimal amount;
+}
---
+++ cloud-provider-payment8001/src/main/java/com/ncamc/cloud/mapper/PayMapper.java
@@ -0,0 +1,7 @@
+package com.ncamc.cloud.mapper;
+
+import com.ncamc.cloud.entities.Pay;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface PayMapper extends Mapper<Pay> {
+}
