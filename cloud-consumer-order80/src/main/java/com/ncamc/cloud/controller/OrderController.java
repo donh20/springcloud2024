1493f2f08a0e6770fd4df16431b608afb282b190
haodong <haodong@ncamc.com.cn>
2024-08-23 14:39:59 +0800

    RestTemplateConfig开启loadbalance特性，否则会报错

--- cloud-consumer-order80/src/main/java/com/ncamc/cloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/ncamc/cloud/controller/OrderController.java
@@ -12,7 +12,8 @@ public class OrderController {
     /**
      * url,requestMap,reseponseEntity
      */
-    public static final String Payment_URL = "http://localhost:8001";//硬编码写死
+//    public static final String Payment_URL = "http://localhost:8001";//硬编码写死
+    public static final String Payment_URL = "http://cloud-payment-service";//服务注册中心上的微服务名称
 
     @Resource
     private RestTemplate restTemplate;
