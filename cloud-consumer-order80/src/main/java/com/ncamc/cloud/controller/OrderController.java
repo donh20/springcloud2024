f2659affd150adc8587a708447f271277b12a991
haodong <haodong@ncamc.com.cn>
2024-09-03 16:30:16 +0800

    负载均衡切换为随机

--- cloud-consumer-order80/src/main/java/com/ncamc/cloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/ncamc/cloud/controller/OrderController.java
@@ -3,9 +3,13 @@ package com.ncamc.cloud.controller;
 import com.ncamc.cloud.entity.PayDTO;
 import com.ncamc.cloud.resp.ResultData;
 import jakarta.annotation.Resource;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 public class OrderController {
@@ -45,4 +49,22 @@ public class OrderController {
         return restTemplate.getForObject(Payment_URL+"/pay/get/info",String.class);
     }
 
+    @Resource
+    private DiscoveryClient discoveryClient;
+    @GetMapping("/consumer/discovery")
+    public String discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            System.out.println(element);
+        }
+
+        System.out.println("===================================");
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("cloud-payment-service");
+        for (ServiceInstance element : instances) {
+            System.out.println(element.getServiceId()+"\t"+element.getHost()+"\t"+element.getPort()+"\t"+element.getUri());
+        }
+
+        return instances.get(0).getServiceId()+":"+instances.get(0).getPort();
+    }
 }
